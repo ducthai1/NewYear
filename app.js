847141f2bd91aaae46819611c76648a7f0fa5c41
Thái Tăng Đức <thaitangduc1@gmail.com>
2023-01-20 23:57:57 +0700

    text, animation,

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-var fut = new Date("jan 12, 2023 13:29:00").getTime()
-let x = setInterval(function stime(){
-    var now = new Date().getTime()
-    var D = fut - now
-    var days = Math.floor(D/(1000*60*60*24))
-    var hours = Math.floor(D/(1000*60*60))
-    var minutes = Math.floor(D/(1000*60))
-    var seconds = Math.floor(D/(1000))
-    hours %=24
-    minutes %=60
-    seconds %=60
-    document.getElementById("days").innerText = days
-    document.getElementById("hours").innerText = hours
-    document.getElementById("minutes").innerText = minutes
-    document.getElementById("seconds").innerText = seconds
+var DateExpired = new Date("jan 22, 2023 00:00:00").getTime();
+let x = setInterval(function stime() {
+  var CurrentDate = new Date().getTime();
+  var DayRemaining = DateExpired - CurrentDate;
+  var days = Math.floor(DayRemaining / (1000 * 60 * 60 * 24));
+  var hours = Math.floor(DayRemaining / (1000 * 60 * 60));
+  var minutes = Math.floor(DayRemaining / (1000 * 60));
+  var seconds = Math.floor(DayRemaining / 1000);
 
-    if(D<0){
-        clearInterval(x);
-        window.location.href="intro.html"
-    }
+  hours %= 24;
+  minutes %= 60;
+  seconds %= 60;
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  if (D < 0) {
+    clearInterval(x);
+    window.location.href = "intro.html";
+  }
 }, 0.1);
